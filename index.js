1bef91ee18a28ec5973c1402a86a5c01b95ba4c0
Leandro <leandrosarkis1602@yahoo.com>
2024-09-10 18:03:11 -0300

    menus da aplicação

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-const start = () => {
-    let opcao = "sair";
-    switch(opcao) {
-        case "cadastrar":
-            console.log("vamos cadastrar");
-            break;
-        case "listar":
-            console.log("vamos listar");
-            break;
-        case "sair":
-            return;
+const { select } = require('@inquirer/prompts')
+
+const start = async () => {
+    while(true) {
+        const option = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
+        switch(option) {
+            case "cadastrar":
+                console.log("vamos cadastrar");
+                break;
+            case "listar":
+                console.log("vamos listar");
+                break;
+            case "sair":
+                return;
+        }
     }
 }
 
